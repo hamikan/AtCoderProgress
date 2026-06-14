02a352ccf3be355bcc38abf50d8948b8af35b8f3
hamikan <hamikan.422@gmail.com>
2026-06-14 18:40:00 +0900

    refactor: rename solution route param

--- src/app/(main)/solutions/[solutionId]/params.test.ts
+++ src/app/(main)/solutions/[solutionId]/params.test.ts
@@ -3,14 +3,14 @@ import test from 'node:test';
 
 import { normalizeSolutionRouteParams } from './params';
 
-test('normalizeSolutionRouteParams maps the legacy problemId segment to solutionId', () => {
-  assert.deepEqual(normalizeSolutionRouteParams({ problemId: 'solution_123' }), {
+test('normalizeSolutionRouteParams keeps a valid solutionId segment', () => {
+  assert.deepEqual(normalizeSolutionRouteParams({ solutionId: 'solution_123' }), {
     solutionId: 'solution_123',
   });
 });
 
 test('normalizeSolutionRouteParams rejects invalid route params', () => {
-  assert.throws(() => normalizeSolutionRouteParams({ problemId: '../solution' }), {
+  assert.throws(() => normalizeSolutionRouteParams({ solutionId: '../solution' }), {
     message: 'Invalid solution ID',
   });
 });
--- src/app/(main)/solutions/[solutionId]/params.ts
+++ src/app/(main)/solutions/[solutionId]/params.ts
@@ -1,13 +1,13 @@
 import { normalizeSolutionId } from '@/lib/validation/solution-input';
 
 interface SolutionRouteParamsInput {
-  problemId: string;
+  solutionId: string;
 }
 
 export function normalizeSolutionRouteParams(params: SolutionRouteParamsInput): {
   solutionId: string;
 } {
   return {
-    solutionId: normalizeSolutionId(params.problemId),
+    solutionId: normalizeSolutionId(params.solutionId),
   };
 }
